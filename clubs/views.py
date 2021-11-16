074d719af3d7ef89b85853c36f676d416e7ad141
Xiangyi Liu <xiangyi.1.liu@kcl.ac.uk>
2021-11-16 11:18:03 +0000

    Make some changes for the welocme page

--- clubs/views.py
+++ clubs/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
-def login(request):
-    return render(request, 'login.html')
-def signup(request):
-    return render(request, 'signup.html')
+def log_in(request):
+    return render(request, 'log_in.html')
+def sign_up(request):
+    return render(request, 'sign_up.html')
