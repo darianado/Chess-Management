863a65c7523f6ba027f864a3f2ea33d3a2ace399
Krishi Wali <krishi.wali@kcl.ac.uk>
2021-11-16 11:58:39 +0000

    Add models to admin page

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from clubs.models import User, Club, Members
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for users"""
+    list_display = [
+        'username', 'first_name', 'last_name', 'email', 'is_active', 'bio', 'chess_experience_level', 'personal_statement'
+    ]
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for clubs"""
+    list_display = [
+        'club_name', 'location', 'description'
+    ]
+
+@admin.register(Members)
+class MemberAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for members"""
+    list_display = [
+        'club', 'user', 'role'
+    ]
