f80c00291c6db36a3e7fae2a3881f19c74913c0e
Krishi Wali <krishi.wali@kcl.ac.uk>
2021-11-18 00:55:44 +0000

    Make chess_experience_level an optional field

--- clubs/models.py
+++ clubs/models.py
@@ -41,6 +41,8 @@ class User(AbstractUser):
 
     chess_experience_level = models.IntegerField(
         unique=False,
+        blank=True,
+        default=1,
         validators=[
             MinValueValidator(1),
             MaxValueValidator(5)
