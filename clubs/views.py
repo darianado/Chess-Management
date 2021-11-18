7c376db8e62c77573adf971db48725699b4af038
Krishi Wali <krishi.wali@kcl.ac.uk>
2021-11-18 00:58:22 +0000

    Add user profile
    All fields are shown currently only if the user is viewing their own profile.
    If someone is viewing someone else's profile they will see only their gravatar, name and bio.
    Add an edit profile button. This only appears when a user views their own profile. Currently it only redirects to the same page.

--- clubs/views.py
+++ clubs/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+
+from clubs.models import User
 
 # Create your views here.
 def welcome(request):
@@ -7,3 +10,16 @@ def login(request):
     return render(request, 'login.html')
 def signup(request):
     return render(request, 'signup.html')
+
+#@login_required
+def user_profile(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return redirect()
+    else:
+        return render(request, "show_user.html", {"logged_in_user": request.user, "user_profile": user})
+
+#@login_required
+def edit_profile(request, user_id):
+    return redirect("user_profile", user_id)
