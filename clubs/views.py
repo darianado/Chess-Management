f3a8ba679cdbcd89aadd090ec6ea0271dbda58e2
Krishi Wali <krishi.wali@kcl.ac.uk>
2021-11-24 10:35:26 +0000

    Personal information on someone's profile is now additionally visible to officers/owners of a club the user is in

--- clubs/views.py
+++ clubs/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from clubs.forms import LogInForm, SignUpForm, EditProfileForm
-from clubs.models import User
+from clubs.models import Members, User
 from django.contrib.auth import authenticate, login, logout
+from django.db.models import Q
 
 # Create your views here.
 def welcome(request):
@@ -42,7 +43,30 @@ def show_user(request, user_id):
     except User.DoesNotExist:
         return redirect()
     else:
-        return render(request, "show_user.html", {"logged_in_user": request.user, "user_profile": user})
+        show_personal_information = False
+        if request.user == user:
+            show_personal_information = True
+            own_profile = True
+        else:
+            own_profile = False
+            # Get the clubs of the logged in user where they are an officer or owner
+            logged_in_user_clubs = [member.club for member in Members.objects.filter(Q(user=request.user) & (Q(role=2) | Q(role=1)))]
+
+            # Check if the user who's profile is being viewed has any clubs in common with the logged in user
+            # where the logged in user is an officer or owner
+            if Members.objects.filter(user=user, club__in=logged_in_user_clubs).exists():
+                show_personal_information = True
+
+
+        return render(
+            request,
+            "show_user.html",
+            {
+                "show_personal_info": show_personal_information,
+                "own_profile": own_profile,
+                "user_profile": user
+            }
+        )
 
 #@login_required
 def profile(request):
