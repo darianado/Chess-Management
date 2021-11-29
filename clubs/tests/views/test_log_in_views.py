fe05150c6ae440ebeb17d4f673f6f20a798a1bb8
Mary <86668599+MaryJml@users.noreply.github.com>
2021-11-29 13:25:12 +0000

    Update test_log_in_views.py

--- clubs/tests/views/test_log_in_views.py
+++ clubs/tests/views/test_log_in_views.py
@@ -51,7 +51,7 @@ class LogInViewTestCase(TestCase, LogInTester):
         form_input = {'email': 'xiangyi@gmail.com', 'password': 'Password123'}
         response = self.client.post(self.url, form_input, follow = True)
         self.assertTrue(self._is_logged_in())
-        response_url = reverse('welcome')
+        response_url = reverse('home')
         self.assertRedirects(response, response_url, status_code = 302, target_status_code = 200)
         self.assertTemplateUsed(response, 'welcome.html')
 
