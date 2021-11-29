3cb20ffbaf9281ad548504ca53ba12a195fea1c1
Mary <86668599+MaryJml@users.noreply.github.com>
2021-11-29 13:29:00 +0000

    Update test_log_in_views.py

--- clubs/tests/views/test_log_in_views.py
+++ clubs/tests/views/test_log_in_views.py
@@ -53,7 +53,7 @@ class LogInViewTestCase(TestCase, LogInTester):
         self.assertTrue(self._is_logged_in())
         response_url = reverse('home')
         self.assertRedirects(response, response_url, status_code = 302, target_status_code = 200)
-        self.assertTemplateUsed(response, 'welcome.html')
+        self.assertTemplateUsed(response, 'home.html')
 
 
     def test_valid_log_in_by_inactive_user(self):
