a64e735d310e71ebcc016572da9484d9a227bcaf
Dariana <darianadorin@yahoo.com>
2021-11-29 16:09:27 +0000

    Fix test

--- clubs/tests/views/test_show_club_view.py
+++ clubs/tests/views/test_show_club_view.py
@@ -25,7 +25,7 @@ class ShowUserTest(TestCase):
         response = self.client.get(url, follow=True)
         response_url = reverse('club_list')
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
-        self.assertTemplateUsed(response, 'user_list.html')
+        self.assertTemplateUsed(response, 'club_list.html')
         
 
         # tests for apply button and leave button
