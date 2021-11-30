a39604bb18c9c632f8e5a238afc78f5b02f515df
Krishi Wali <krishi.wali@kcl.ac.uk>
2021-11-30 20:27:43 +0000

    Use fixtures in log_in view test

--- clubs/tests/views/test_log_in_views.py
+++ clubs/tests/views/test_log_in_views.py
@@ -6,24 +6,16 @@ from clubs.models import User
 from clubs.tests.helper import LogInTester
 from django.contrib import messages
 
-
-
 class LogInViewTestCase(TestCase, LogInTester):
+
+    fixtures = [
+        "clubs/tests/fixtures/default_user_john.json"
+    ]
+
     """"Tests of the log in view."""
     def setUp(self):
         self.url = reverse('log_in')
-        self.user = User.objects.create_user(
-            first_name='Xiangyi',
-            last_name='Liu',
-            email='xiangyi@gmail.com',
-            password='Password123',
-            bio='Hi!! My name is Xiangyi.',
-            chess_experience_level='1',
-            personal_statement='Hello!',
-            is_active=True,
-        )
-
-
+        self.user = User.objects.get(email="johndoe@example.org")
 
     def test_log_in_url(self):
         self.assertEqual(self.url, '/log_in/' )
@@ -36,9 +28,11 @@ class LogInViewTestCase(TestCase, LogInTester):
         self.assertTrue(isinstance(form, LogInForm))
         self.assertFalse(form.is_bound)
 
-
     def test_unsuccessful_log_in(self):
-        form_input = {'email': 'xiangyi@gmail.com', 'password': 'WrongPassword123'}
+        form_input = {
+            'email': 'johndoe@example.org',
+            'password': 'WrongPassword123'
+        }
         response = self.client.post(self.url, form_input)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'log_in.html')
@@ -48,18 +42,23 @@ class LogInViewTestCase(TestCase, LogInTester):
         self.assertFalse(self._is_logged_in())
 
     def test_successful_log_in(self):
-        form_input = {'email': 'xiangyi@gmail.com', 'password': 'Password123'}
-        response = self.client.post(self.url, form_input, follow = True)
+        form_input = {
+            'email': 'johndoe@example.org',
+            'password': 'Password123'
+        }
+        response = self.client.post(self.url, form_input, follow=True)
         self.assertTrue(self._is_logged_in())
         response_url = reverse('home')
-        self.assertRedirects(response, response_url, status_code = 302, target_status_code = 200)
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, 'home.html')
-
 
     def test_valid_log_in_by_inactive_user(self):
         self.user.is_active = False
         self.user.save()
-        form_input = { 'email': 'xiangyi@gmail.com', 'password': 'Password123' }
+        form_input = {
+            'email': 'johndoe@example.org',
+            'password': 'Password123'
+        }
         response = self.client.post(self.url, form_input, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'log_in.html')
