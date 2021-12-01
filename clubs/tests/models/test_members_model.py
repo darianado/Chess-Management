381250f06520b98f89e94b61723e9a9b408cf677
Krishi Wali <krishi.wali@kcl.ac.uk>
2021-12-01 16:51:45 +0000

    Update Members model tests to use fixtures

--- clubs/tests/models/test_members_model.py
+++ clubs/tests/models/test_members_model.py
@@ -2,42 +2,27 @@ from django.test import TestCase
 from clubs.models import Club, Members, User
 from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
+from django.db import transaction
 
 from clubs.helpers import Role
 
 class MembersModelTest(TestCase):
+
+    fixtures = [
+        "clubs/tests/fixtures/default_user_john.json",
+        "clubs/tests/fixtures/default_user_jane.json",
+        "clubs/tests/fixtures/default_club_hamersmith.json",
+        "clubs/tests/fixtures/default_club_hame.json",
+        "clubs/tests/fixtures/default_member_john_hame.json",
+    ]
+
     def setUp(self):
-        self.user = User.objects.create_user(
-            first_name='John',
-            last_name='Doe',
-            email='johndoe@example.org',
-            password='Password123',
-            bio='The quick brown fox jumps over the lazy dog.',
-            chess_experience_level=1,
-            personal_statement='nu mi place sa joc sah'
-        )
-
-        self.user2 = User.objects.create_user(
-            first_name='Jane',
-            last_name='Doe',
-            email='janedoe@example.org',
-            password='Password123',
-            bio='The quick brown fox jumps over the lazy dog.',
-            chess_experience_level=1,
-            personal_statement='nu mi place sa joc sah'
-        )
-
-        self.club = Club.objects.create(
-            club_name="Cool kids club",
-            location="Somewhere",
-            description="Cool kids only"
-        )
-
-        self.member = Members.objects.create(
-            club=self.club,
-            user=self.user,
-            role=Role.OWNER
-        )
+        self.userJohn = User.objects.get(email="johndoe@example.org")
+        self.userJane = User.objects.get(email="janedoe@example.org")
+        self.clubHame = Club.objects.get(club_name="Hame Chess Club")
+        self.member = Members.objects.get(club=self.clubHame, user=self.userJohn)
+        self.member.role = 1
+        self.member.save()
     
     def test_valid_member(self):
         self._assert_member_is_valid()
@@ -69,17 +54,28 @@ class MembersModelTest(TestCase):
     def test_user_is_only_a_member_of_a_club_exactly_once(self):
         with self.assertRaises(IntegrityError):
             Members.objects.create(
-                club=self.club,
-                user=self.user
+                club=self.clubHame,
+                user=self.userJohn
             )
 
     def test_a_club_cannot_have_more_than_1_owner(self):
         with self.assertRaises(IntegrityError):
-            Members.objects.create(
-                club=self.club,
-                user=self.user2,
-                role=Role.OWNER
-            )
+            with transaction.atomic():
+                Members.objects.create(
+                    club=self.clubHame,
+                    user=self.userJane,
+                    role=Role.OWNER
+                )
+
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                member = Members.objects.create(
+                    club=self.clubHame,
+                    user=self.userJane,
+                    role=Role.MEMBER
+                )
+                member.role = 1
+                member.save()
 
     def _assert_member_is_valid(self):
         try:
