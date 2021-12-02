6047b5219021aa7ae602de050bf27399572ba8ed
Krishi Wali <krishi.wali@kcl.ac.uk>
2021-12-02 23:49:52 +0000

    Update log in form tests to use fixtures

--- clubs/tests/forms/test_log_in_form.py
+++ clubs/tests/forms/test_log_in_form.py
@@ -7,20 +7,14 @@ from django.urls import reverse
 from clubs.tests.helper import LogInTester
 
 class LogInFormTestCase(TestCase, LogInTester):
+
+    fixtures = [
+        "clubs/tests/fixtures/default_user_john.json"
+    ]
+
     """Unit tests of the log in form."""
     def setUp(self):
         self.url = reverse('log_in')
-        User.objects.create_user(
-            first_name='Xiangyi',
-            last_name='Liu',
-            email='xiangyi@gmail.com',
-            password='Password123',
-            bio='Hi!! My name is Xiangyi.',
-            chess_experience_level='1',
-            personal_statement='Hello everyone!'
-        )
-
-
 
     def test_log_in_url(self):
         self.assertEqual(self.url, '/log_in/' )
@@ -34,7 +28,7 @@ class LogInFormTestCase(TestCase, LogInTester):
         self.assertFalse(form.is_bound)
 
     def test_unsuccessful_log_in(self):
-        form_input = {'email': 'xiangyi@gmail.com', 'password': 'WrongPassword123'}
+        form_input = {'email': 'johndoe@example.org', 'password': 'WrongPassword123'}
         response = self.client.get(self.url, form_input)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'log_in.html')
@@ -44,7 +38,7 @@ class LogInFormTestCase(TestCase, LogInTester):
         self.assertFalse(self._is_logged_in())
 
     def test_successful_log_in(self):
-        form_input = {'email': 'xiangyi@gmail.com', 'password': 'Password123'}
+        form_input = {'email': 'johndoe@example.org', 'password': 'Password123'}
         response = self.client.post(self.url, form_input, follow = True)
         self.assertTrue(self._is_logged_in())
         response_url = reverse('home')
