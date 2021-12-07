4958565962e07b5d6c08c50ae0f9d4257f45dd6d
Xiangyi Liu <xiangyi.1.liu@kcl.ac.uk>
2021-12-07 12:33:52 +0000

    Put other seed files into one

--- clubs/management/commands/member_unseed.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from clubs.models import Members
-
-class Command(BaseCommand):
-      """The database member unseeder."""
-
-      def handle(self, *args, **options):
-          Members.objects.all().delete()
