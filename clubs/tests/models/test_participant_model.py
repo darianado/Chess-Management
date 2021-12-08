eea8a2e0d95ae158a465f4ab6327022f4be6e487
Krishi Wali <krishi.wali@kcl.ac.uk>
2021-12-08 18:51:47 +0000

    Complete implementations of participant model tests
    Fixtures still need to be created

--- clubs/tests/models/test_participant_model.py
+++ clubs/tests/models/test_participant_model.py
@@ -6,43 +6,52 @@ class ParticipantModelTest(TestCase):
     fixtures = []
 
     def setUp(self):
-        pass
+        self.participantJohn = None
+        self.participantJane = None
 
     def test_tournament_cannot_be_empty(self):
-        pass
+        self.participantJohn.tournament = None
+        self._assert_participant_is_invalid()
 
     def test_member_cannot_be_empty(self):
-        pass
+        self.participantJohn.member = None
+        self._assert_participant_is_invalid()
 
     def test_score_cannot_be_empty(self):
-        pass
+        self.participantJohn.score = None
+        self._assert_participant_is_invalid()
 
     def test_is_active_cannot_be_empty(self):
-        pass
+        self.participantJohn.is_active = None
+        self._assert_participant_is_invalid()
 
 
 
     def test_score_need_not_be_unique(self):
-        pass
+        self.participantJohn.score = self.participantJane.score
+        self._assert_participant_is_valid()
 
     def test_is_active_need_not_be_unique(self):
-        pass
+        self.participantJohn.is_active = self.participantJane.is_active
+        self._assert_participant_is_valid()
 
 
 
     def test_score_can_be_0(self):
-        pass
+        self.participantJohn.score = 0
+        self._assert_participant_is_valid()
 
     def test_score_cannot_be_less_than_0(self):
-        pass
+        self.participantJohn.score = -1
+        self._assert_participant_is_invalid()
 
 
     def _assert_participant_is_valid(self):
         try:
-            self.participant.full_clean()
+            self.participantJohn.full_clean()
         except (ValidationError):
             self.fail('Test participant should be valid')
 
     def _assert_participant_is_invalid(self):
         with self.assertRaises(ValidationError):
-            self.participant.full_clean()
+            self.participantJohn.full_clean()
