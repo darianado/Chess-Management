644f9d913cfa879d0a3d985e3d6f205cf97a1f47
Dariana <darianadorin@yahoo.com>
2021-12-16 21:34:15 +0000

    coverage apply tournament view

--- clubs/tests/views/test_apply_tournament_view.py
+++ clubs/tests/views/test_apply_tournament_view.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 from clubs.models import User, Membership, Club, Tournament, Participant
+from django.utils import timezone
+from datetime import datetime, time, timedelta
 
 
 class TournamentApplyTest(TestCase):
@@ -19,12 +21,18 @@ class TournamentApplyTest(TestCase):
     def setUp(self):
         super(TestCase, self).setUp()
         self.club = Club.objects.get(club_name="Hame Chess Club")
+
         self.tournament = Tournament.objects.get(name="Yetti")
+        self.tournament.deadline = datetime.isoformat(datetime.now(tz=timezone.utc) + timedelta(days=1))
+        self.tournament.save()
+
         self.userGreta = User.objects.get(email="greatdoe@example.org")
         self.userMary = User.objects.get(email="marydoe@example.org")
         self.userPrada = User.objects.get(email="pradadoe@example.org")
+
         self.member = Membership.objects.get(user=self.userPrada, club=self.club)
         self.participant = Participant.objects.get(member=self.member, tournament=self.tournament)
+
         self.url = reverse('apply_to_tournament', kwargs={'tournament_id': self.tournament.id})
 
     def test_apply_to_club_url(self):
@@ -44,8 +52,6 @@ class TournamentApplyTest(TestCase):
             fetch_redirect_response=True
         )
         self.assertTemplateUsed(response, 'show_tournament.html')
-        # messages_list = list(response.context["messages"])
-        # self.assertEqual(len(messages_list), 1)
 
     def test_successful_apply_to_tournament(self):
         self.participant.delete()
@@ -62,8 +68,6 @@ class TournamentApplyTest(TestCase):
             fetch_redirect_response=True
         )
         self.assertTemplateUsed(response, 'show_tournament.html')
-        # messages_list = list(response.context["messages"])
-        # self.assertEqual(len(messages_list), 1)
 
     def test_unsuccessful_apply_to_tournament_when_post(self):
         self.participant.delete()
@@ -80,5 +84,21 @@ class TournamentApplyTest(TestCase):
             fetch_redirect_response=True
         )
         self.assertTemplateUsed(response, 'show_tournament.html')
-        # messages_list = list(response.context["messages"])
-        # self.assertEqual(len(messages_list), 1)
+        
+    def test_unsuccessful_apply_to_tournament_by_deadline(self):
+        self.tournament.deadline=datetime.isoformat(datetime.now(tz=timezone.utc) - timedelta(days=1))
+        self.tournament.save()
+        self.participant.delete()
+        self.client.login(email=self.userMary.email, password="Password123")
+        self.assertTrue(self.client.login(email=self.userMary.email, password="Password123"))
+        participant_count_before = Participant.objects.count()
+        response = self.client.post(self.url, follow=True)
+        participant_count_after = Participant.objects.count()
+        self.assertEqual(participant_count_after, participant_count_before)
+        response_url = reverse('show_tournament', kwargs={'tournament_id': self.tournament.id})
+        self.assertRedirects(
+            response, response_url,
+            status_code=302, target_status_code=200,
+            fetch_redirect_response=True
+        )
+        self.assertTemplateUsed(response, 'show_tournament.html')
