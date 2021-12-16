f029cbf9b66bc7153549a39d9376e4b55a08b09b
nicole.lehchevska <nicole.lehchevska@kcl.ac.uk>
2021-12-16 13:47:22 +0000

    Add a fix to create tournament form

--- clubs/views.py
+++ clubs/views.py
@@ -10,6 +10,7 @@ from django.http import HttpResponseForbidden, request
 from django.contrib.auth.decorators import login_required
 from clubs.helpers import Role, Status
 from clubs.decorators import login_prohibited, minimum_role_required
+from datetime import datetime
 
 @login_prohibited(redirect_location="dashboard")
 def welcome(request):
@@ -473,20 +474,23 @@ def set_match_result(request, match_id):
 
 
 @login_required(redirect_field_name="")
+# TODO test for minimum role member
 def apply_to_tournament(request, tournament_id ):
+
     tournament = Tournament.objects.get(id=tournament_id)
     club = tournament.club
     user = request.user
     member = Membership.objects.get(user=user,club=club)
     member_in_club = Membership.get_member_role(user,club)
-    if tournament.participants.count() < tournament.capacity:
-        if request.method == 'GET':
-            Participant.objects.create(
-                    tournament = tournament,
-                    member = member,
-            )
-        else:
-            return redirect('show_tournament', tournament.id)
+    if datetime.now(tz=timezone.utc) < tournament.deadline:
+        if tournament.participants.count() < tournament.capacity:
+            if request.method == 'GET':
+                Participant.objects.create(
+                        tournament = tournament,
+                        member = member,
+                )
+            else:
+                return redirect('show_tournament', tournament.id)
 
     else:
         # TODO add messages
