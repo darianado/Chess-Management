882c323d152f88eec4c9e22e2b5f8096bddede0d
Krishi Wali <krishi.wali@kcl.ac.uk>
2021-12-16 20:35:48 +0000

    More minor fixes to reflect changes in migrations

--- clubs/models.py
+++ clubs/models.py
@@ -255,7 +255,7 @@ class Tournament(models.Model):
 
     organiser = models.ForeignKey(Membership, on_delete=models.CASCADE, related_name="organiser")
 
-    coorganisers = models.ManyToManyField(Membership)
+    coorganisers = models.ManyToManyField(Membership, blank=True)
 
     club = models.ForeignKey(Club, on_delete=models.CASCADE)
 
@@ -302,7 +302,6 @@ class Tournament(models.Model):
 
 class Participant(models.Model):
     class Meta:
-        ordering=["-score"]
         constraints=[
             models.UniqueConstraint(fields=["tournament", "member"], name="Participant of a tournament only once"),
         ]
@@ -310,15 +309,6 @@ class Participant(models.Model):
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
 
     member = models.ForeignKey(Membership, on_delete=models.CASCADE)
-
-    score = models.FloatField(
-        unique=False,
-        blank=False,
-        default=0,
-        validators=[
-            MinValueValidator(0)
-        ]
-    )
 
     is_active = models.BooleanField(
         unique=False,
